060fc075907f7cddcab8c14bc24f6e7b2b2a52f0
yan2004 <yanj2004@gmail.com>
2020-11-24 15:58:57 -0500

    form modification

--- Controler.class.php
+++ Controler.class.php
@@ -39,6 +39,15 @@ class Controler
 				case 'boireBouteilleCellier':
 					$this->boireBouteilleCellier();
 					break;
+				case 'modifierBouteilleCellier':
+					$this->modifierBouteilleCellier();
+					break;
+				case 'formModificationBtl':
+				 	$this->formModificationBtl();
+					 break;
+				case 'sauvegardeBouteille':
+					$this->sauvegardeBouteille();
+					break;
 				case 'accueilUsager':
 					$this->accueilUsager();
 				    break;
@@ -172,6 +181,42 @@ class Controler
 			echo json_encode($resultat);
 		}
 
+		private function modifierBouteilleCellier()
+		{
+			$bte = new Bouteille();
+			$data = $bte->getListeBouteilleCellierById($_GET['id']);
+			echo json_encode($data);
+
+			//  include("vues/entete.php");
+			//  include("vues/modificationBtl.php");
+			//  include("vues/pied.php");
+			
+		}
+
+		 private function formModificationBtl()
+		 {
+			
+			   $data =json_decode($_GET['dataBtls'], true);
+			   //var_dump($data[0]);
+		
+			   include("vues/entete.php");
+			   include("vues/modificationBtl.php");
+			   include("vues/pied.php");
+			  
+			   
+		 }
+
+		private function sauvegardeBouteille()
+		{
+			$requestPayload = file_get_contents('php://input');
+			$object = json_decode($requestPayload, true);
+			//var_dump($object);
+
+			$bte = new Bouteille();
+			$resultat = $bte->modificationInfoBtl($object['btlIdPK'],$object['date_achat'],$object['garde'],$object['notes'],$object['prix'],$object['quantite'],$object['millesime']);
+
+		}
+
 		private function modifierCompte()
 		{
 			$usager = new Usager();
@@ -213,6 +258,8 @@ class Controler
 
 			$this->accueil();
 		}
+
+
 }
 ?>
 
---
+++ vues/modificationBtl.php
@@ -0,0 +1,40 @@
+<div class="modificationBtl">
+
+    <h2>Modifier Bouteille</h2>
+        
+        <form action="" method="post" class="modificationBtl" name="fModificationBtl">
+                <input type="hidden" name="btlIdPK" value="<?php echo $data[0]['id']; ?>">
+                <input type="hidden" name="nomIdFK" value="<?php echo $data[0]['id_bouteille']; ?>">
+
+                <label for="nom">Nom</label>
+                <input type="text" name="nom" id="nom" value="<?php echo $data[0]['nom']; ?>" readonly="true"/>
+                <span id="errNom"></span>
+            
+                <label for="millesime">Millesime</label>
+                <input type="text" name="millesime" id="millesime" value="<?php echo $data[0]['millesime']; ?>">
+                <span id="errMillesime"></span>
+            
+                <label for="quantite">Quantite</label>
+                <input type="text" name="quantite" id="quantite" value="<?php echo $data[0]['quantite']; ?>">
+                <span id="errQuantite"></span>
+
+                <label for="date_achat">Date achat</label>
+                <input type="date" name="date_achat" id="date_achat" value="2020-10-01">
+                <span id="errDateAchat"></span>
+
+                <label for="prix">Prix</label>
+                <input type="text" name="prix" id="prix" value="<?php echo $data[0]['prix']; ?>">
+                <span id="errPrix"></span>
+
+                <label for="garde">Garde</label>
+                <input type="date" name="garde" id="garde" value="2020-10-01" value="<?php echo $data[0]['garde_jusqua']; ?>">
+                <span id="errGarde"></span>
+
+                <label for="notes">Notes</label>
+                <input type="text" name="notes" id="notes" value="<?php echo $data[0]['notes']; ?>">
+                <span id="errNotes"></span>
+                
+                <button type="submit" value="Modifier" class="btnModifierBtl">MODIFIER</button>
+        </form>
+    
+</div>
