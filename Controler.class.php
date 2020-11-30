c717aa5cd2f7ce04f902b0df9c8bb16bca3964fe
yan2004 <yanj2004@gmail.com>
2020-11-30 14:32:04 -0500

    commenter boutons Gérer Usager et Collection pour admin panel page

--- Controler.class.php
+++ Controler.class.php
@@ -173,16 +173,22 @@ class Controler
 		private function accueilUsager()
 		{
 			if(isset($_SESSION['courriel'])){
-				$bte = new Bouteille();
-				$data = $bte->getListeBouteilleCellier();
-
-				// pour afficher le nom d'usager
-				$usager = new Usager();
-				$dataUsager = $usager->getUserByCourriel($_SESSION['courriel']);
-
-				include("vues/entete.php");
-				include("vues/cellier.php");
-				include("vues/pied.php");  
+				if($_SESSION['courriel'] == "admin_pw2@cmaisonneuve.qc.ca"){
+					include("vues/enteteAdmin.php");
+					include("vues/mainAdmin.php");
+					include("vues/pied.php");  
+				}else{
+					$bte = new Bouteille();
+					$data = $bte->getListeBouteilleCellier();
+	
+					// pour afficher le nom d'usager
+					$usager = new Usager();
+					$dataUsager = $usager->getUserByCourriel($_SESSION['courriel']);
+	
+					include("vues/entete.php");
+					include("vues/cellier.php");
+					include("vues/pied.php");  
+				}
 			}else{
 				$this->accueil();
 			}  
