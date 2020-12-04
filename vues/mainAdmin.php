128446387aff8ebc0ebb42db0ee2eeddcd36ee89
Marianne Soucy <soucy.marianne@protonmail.com>
2020-12-04 14:19:28 -0500

    legère amélioration du positionnement et taille des icones + et - pour mobile

--- vues/mainAdmin.php
+++ vues/mainAdmin.php
@@ -2,7 +2,7 @@
    <div class="main-title">
        <div class="main-title-welcome">
             <h1>Admin Panel
-            &#8239;:
+            <!-- &#8239;: -->
             </h1>
             <div class="main-title-call-action">
                 <button class="btn-call-action" id="btnImportation">Importer les bouteilles</button>
